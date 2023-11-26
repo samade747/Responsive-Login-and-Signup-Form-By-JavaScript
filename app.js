add2699f279536cc2441343421aa137923d1a877
samade747 <samad.e747@gmail.com>
2023-11-26 15:28:00 +0500

    3:27PM 26 11 2023

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 function registerNow(){
+    event.preventDefault();
     var fullName = document.getElementById('fullName');
     var number = document.getElementById('Number');
     var email = document.getElementById('Email');
     var password = document.getElementById('Password');
     var confirmPassword = document.getElementById('ConfirmPassword');
 
-    console.log(fullName.value, )
+    console.log(fullName.value, number.value, email.value, password.value, confirmPassword.value);
+
+    var userData = {
+        fullName : fullName.value,
+        number : number.value,
+        email : email.value,
+        password : password.value,
+        confirmPassword : confirmPassword.value
+
+    }
+    console.log(userData);
+
+    localStorage.setItem("userData", JSON.stringify(userData));
+
+    var getUserData = JSON.parse(localStorage.getItem("userData"));
+    console.log(getUserData);
+
+
+
+    
+    
 }
