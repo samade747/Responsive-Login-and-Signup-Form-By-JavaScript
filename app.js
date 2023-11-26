6b47467b367b6b897adc8b59c4db6f6ea83d88e7
samade747 <samad.e747@gmail.com>
2023-11-26 13:25:40 +0500

    1:25PM 26112023

---
+++ app.js
@@ -0,0 +1,9 @@
+function registerNow(){
+    var fullName = document.getElementById('fullName');
+    var number = document.getElementById('Number');
+    var email = document.getElementById('Email');
+    var password = document.getElementById('Password');
+    var confirmPassword = document.getElementById('ConfirmPassword');
+
+    console.log(fullName.value, )
+}
